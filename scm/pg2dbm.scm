c611b6715ab545fa056f39779b5114a90f0276f6
Michal Maruska <mmaruska@gmail.com>
2013-08-07 21:42:58 +0200

    Comment on wrong API ... pg2dbm
    Change-Id: Id47c90656b587f0b366bd17c92f3e6c53de641c0

--- scm/pg2dbm.scm
+++ scm/pg2dbm.scm
@@ -1,42 +1,35 @@
 
-;; copy  from PG to BDB
+;; copy  from PG to dbm (could be BDB)
 
 ;; fixme: change name!
 (define-module pg2dbm
   (export
-   copy-pg->dbm
-   )
+   copy-pg->dbm)
+
   (use pg)
   (use pg-hi)
 
   (use pg.sql)
-  (use dbm)
-;(use gauche.uvector)
-  )
+  (use dbm))
+
 (select-module pg2dbm)
 
 
+;; todo: @pgconn @query ...
+;; the key & value types must match that of the pg result!
 (define (copy-pg->dbm query db . args)
   (let-optionals* args
-      ((conn (pg-connect ""))           ; fixme!
+      ((conn (pg-connect ""))
        (fields '(0 1)))
-
-    ;; assert
-    ;; the key & value types match that of the pg result!
-    
+    ;; assert: at least 2 columns.
     (let* ((result (pg-exec conn query)))
       ;; copy:
-                                        ;(pg-ntuples result)
       (pg-foreach-result result fields
-        (lambda (key value) 
+        (lambda (key value)
           (dbm-put! db key value)))
-      ;;
+
       ;; (db-sync db 0)
+      ;; close it?
       )))
-
-
-
-
-
 
 (provide "pg2dbm")
