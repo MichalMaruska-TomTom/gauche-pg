da4640e3ef24f81f1e525e2ea056bbb7be58105a
Michal Maruska <mmaruska@gmail.com>
2022-04-05 18:48:02 +0200

    document pg.scm as the wrapper for C

--- scm/pg.scm
+++ scm/pg.scm
@@ -1,12 +1,15 @@
 
 ;; This is the wrapper of C code.
+;; There is also a callback to Scheme: `pg-handle-hook' see ../pg-lib.c
 
 ;; todo: the result of BEGIN indicates, that the handle enters a transaction
 ;; ..... try to detect it!!
 
 (define-module pg
   (export-all)
-  (use gauche.uvector)                  ; fixme: the C code uses it!
+
+  ;; fixme: the C code uses it, but I'm unable to do this from C
+  (use gauche.uvector)
   )
 
 (select-module pg)
@@ -24,15 +27,15 @@
 
 ;;; hook:
 (define (pg-prepare-handle handle)
+  ;; similar to ~/.psqlrc
   (if debug (logformat "pg-prepare-handle\n"))
-  (pg-exec handle
-    "set enable_seqscan to 0;")
-  (pg-exec handle
-    "set DateStyle TO 'European';")
+  ;; tune the planner:
+  (pg-exec handle "set enable_seqscan to 0;")
+  (pg-exec handle "set DateStyle TO 'European';")
   ;(pg-exec handle "set debug_print_plan to on")
   )
 
-;;
+
 (unless (member pg-prepare-handle pg-handle-hook)
   (push! pg-handle-hook pg-prepare-handle))
 
